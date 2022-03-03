aaf13a682d6d7efb6eb120c61202a38b152e0633
nafron <99122428+nafron@users.noreply.github.com>
2022-03-03 17:05:12 +0700

    Add files via upload

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -3,9 +3,9 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, await (await fetch(thumbfoto)).buffer(), `
 ┌〔 Donasi • Emoney 〕
-├ GoPay: 085730903853 [AlyaXd]
-├ Dana: 082221792667 [BOTCAHX]
-├ Pulsa: 081395861695 [BOTCAHX]
+├ GoPay: 081252733108 [Nafron]
+├ Dana: 081252733108 [Nafron]
+├ Pulsa: 081252733108 [Nafron]
 └────
 `.trim(), watermark, 'Menu', '.menu', m)
 handler.help = ['donasi']

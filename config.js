5d37463a41284611cb5658c6dcd024700d8c4c71
nafron <99122428+nafron@users.noreply.github.com>
2022-03-03 19:01:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -48,7 +48,7 @@ global.wait = '「 ⏱️ 」Harap tunggu...'
 global.eror = '「❗」Server ERROR!'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
 global.watermark = '2022 © Nafron' //change the watermark 
-global.image = 'https://telegra.ph/file/0bdd84bd5a8099a789828.jpg' //change the image
+global.image = 'https://telegra.ph/Nafron-03-03' //change the image
 global.thumbfoto = 'https://telegra.ph/file/0bdd84bd5a8099a789828.jpg'
 
 // image
